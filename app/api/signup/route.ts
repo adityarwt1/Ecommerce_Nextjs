642e4541cc0dbd51df5d74648b0551a93eaa0c02
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-05 15:13:23 +0530

    sign api written

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,4 +1,6 @@
-import { connectdb } from "@/lib/mongodb";
+import { connectdb, disconnectdb } from "@/lib/mongodb";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -12,7 +14,38 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    await connectdb()
-    const user = new 
-  } catch (error) {}
+    const hashePassword = await bcrypt.hash(password as string, 5);
+    await connectdb();
+
+    const existingUer = await User.findOne({ email });
+    if (existingUer) {
+      return NextResponse.json(
+        { error: "User already exist" },
+        { status: 409 }
+      );
+    }
+    const user = new User({
+      firstname,
+      lastname,
+      email,
+      phonenumber,
+      password: hashePassword,
+    });
+    await user.save();
+    await disconnectdb();
+    if (user) {
+      return NextResponse.json(
+        { message: "Signup successfully" },
+        { status: 201 }
+      );
+    } else {
+      return NextResponse.json({ error: "Failed to sign in" }, { status: 500 });
+    }
+  } catch (error) {
+    console.log((error as Error).message);
+    return NextResponse.json(
+      { error: "Internal server issue" },
+      { status: 5000 }
+    );
+  }
 }
